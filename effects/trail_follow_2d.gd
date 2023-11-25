e741b5ca97f7796d3b4b8a985f9f2da915834914
Steven Raines <stevenmraines@gmail.com>
2023-11-25 17:09:25 -0500

    Refactor found wind gust effect code so that it makes more sense to me.

--- effects/trail_follow_2d.gd
+++ effects/trail_follow_2d.gd
@@ -1,4 +1,21 @@
 extends PathFollow2D
 class_name TrailFollow2D
 
+# Imagine a Path2D with 4 points (P) like so:
+# P--------P--------P--------P
+#
+# A normal PathFollower2D (F) would start at P1 and traverse to the end to P4 like so:
+#     F--------P--------P--------P
+# --> P--------F--------P--------P
+# --> P--------P--------F--------P
+# --> P--------P--------P--------F
+#
+# But we want a follower that traverses the path with a trail made up of many segments (S):
+#     SSSSSSSSSS--------P--------P--------P
+# --> P--------SSSSSSSSSS--------P--------P
+# --> P--------P--------SSSSSSSSSS--------P
+# --> P--------P--------P--------SSSSSSSSSS
+#
+# Each segment is an instance of this class, and it's trail_offset
+# determines its offset from the lead trail follower
 var trail_offset := 0.0
--- effects/wind_gust.gd
+++ effects/wind_gust.gd
@@ -1,9 +1,9 @@
+# The root Line2D is used to actually draw the trail of path followers
 extends Line2D
 
-export var path_flip_h = false
-export var reverse_path_traversal = false
+export var flip_h = false
 export var line_segments = 20
-export var trail_length = 1.0
+export var trail_spacing = 1.0
 export var trail_speed = 0.001
 export var movement_speed = 200
 export var random_y_offset = 0.0
@@ -12,83 +12,95 @@ export var trail_color_gradient : Gradient
 onready var _path_2d := $Path2D
 onready var _visibility_notifier_2d := $VisibilityNotifier2D
 
-var pf_dict = []
+var trail_followers = []
 var use_gradient = false
 
 
 func _ready():
-	_initialize_path()
-
-
-func _initialize_path():
-	if path_flip_h:
+	if flip_h:
 		flip_path()
 	
 	if random_y_offset > 0.0:
 		randomize_path()
 	
-	init_path_followers()
+	init_trail_followers_and_gradient()
 	
 	if trail_color_gradient != null:
 		use_gradient = true
 	
-	if reverse_path_traversal:
-		for pf in pf_dict:
-			pf.unit_offset = 1.0
+	if flip_h:
+		for follower in trail_followers:
+			follower.unit_offset = 1.0
 	
 # warning-ignore:return_value_discarded
 	_visibility_notifier_2d.connect("screen_exited", self, "remove")
 
 
 func _process(delta):
-	position.x = position.x - movement_speed * delta
+#	position.x = position.x - movement_speed * delta
 	
-	move_path()
+	move_trail_followers()
 	
 	if use_gradient:
-		update_path_gradient()
+		update_line_gradient()
 	
-	draw_path()
+	update_line_points()
 
 
 func flip_path():
+	# The position of the path point
 	var point_pos = []
+	# The left curve control handle of the point
 	var point_in = []
+	# The right curve control handle of the point
 	var point_out = []
 	
-	var curve_points = _path_2d.curve.get_point_count()
+	var path_point_count = _path_2d.curve.get_point_count()
 	
-	for i in range(0, curve_points):
+	# Back up the path points before clearing them
+	for i in range(0, path_point_count):
 		point_pos.append(_path_2d.curve.get_point_position(i))
 		point_in.append(_path_2d.curve.get_point_in(i))
 		point_out.append(_path_2d.curve.get_point_out(i))
 	
+	# Clear the points
 	_path_2d.curve.clear_points()
 	
 	var j = 0
-	for i in range(curve_points - 1, -1, -1):
+	# Loop from num of points - 1 to 0, with a step of -1
+	for i in range(path_point_count - 1, -1, -1):
+		# Add the points back in the reverse order
 		_path_2d.curve.add_point(point_pos[i], -point_in[i], -point_out[i], j)
 		j += 1
 
 
-func move_path():
-	for pf in pf_dict:
-		pf.trail_offset += trail_speed
-		if pf.trail_offset >= 0.0 and pf.trail_offset <= 1.0:
-			pf.unit_offset = pf.trail_offset
+func move_trail_followers():
+	# Every frame, loop over all of the trail_followers
+	# and add trail_speed to their trail_offset
+	for follower in trail_followers:
+		follower.trail_offset += trail_speed
+		# If trail_offset is between 0 and 1, it's now "visible"
+		# on the path and it's unit_offset should be updated
+		if follower.trail_offset >= 0.0 and follower.trail_offset <= 1.0:
+			follower.unit_offset = follower.trail_offset
 		
-		if pf.trail_offset >= 1.0:
-			pf.unit_offset = 1.0
+		# Cap unit_offset at 1, because trail_offset will be larger than
+		# that as items at the end of the trail reach the end of the path
+		if follower.trail_offset >= 1.0:
+			follower.unit_offset = 1.0
 	
-	var completed_offset = 0.0 if reverse_path_traversal else 1.0
-	if pf_dict[0].unit_offset == completed_offset:
+	# If the final trail follower has reached the end of the path, remove the wind gust
+	var completed_offset = 0.0 if flip_h else 1.0
+	var final_follower_index = line_segments - 1 if flip_h else 0
+	if trail_followers[final_follower_index].unit_offset == completed_offset:
 		remove()
 
 
-func update_path_gradient():
+func update_line_gradient():
 	for pcnt in range(line_segments + 1):
 		gradient.colors[pcnt] = trail_color_gradient.interpolate(gradient.offsets[pcnt])
-		gradient.colors[pcnt].a *= trail_color_gradient.interpolate(pf_dict[pcnt].unit_offset).a
+		gradient.colors[pcnt].a *= \
+				trail_color_gradient.interpolate(trail_followers[pcnt].unit_offset).a
 
 
 func randomize_path():
@@ -101,26 +113,38 @@ func randomize_path():
 		_path_2d.curve.set_point_position(i, curve_point)
 
 
-func draw_path():
+# Clear and redraw the updated line every frame by
+# adding each trail follower as a point on the Line2D
+func update_line_points():
 	clear_points()
 	
-	for pf in pf_dict:
-		add_point(pf.global_position)
+	for follower in trail_followers:
+		add_point(follower.global_position)
 
 
-func init_path_followers():
-	for pf_cnt in range(line_segments + 1):
-		var new_pf = TrailFollow2D.new()
-		_path_2d.add_child(new_pf)
-		new_pf.trail_offset = float(pf_cnt) / float(line_segments) * trail_length - trail_length
-		new_pf.loop = false
-		pf_dict.append(new_pf)
+func init_trail_followers_and_gradient():
+	# For 0 through line_segments (ex. 0 through 20)
+	# Add a TrailFollow2D to the Line2D
+	# Set it's trail_offset and add it to the array of trail_followers
+	for i in range(line_segments + 1):
+		var trail_follow_2d = TrailFollow2D.new()
+		_path_2d.add_child(trail_follow_2d)
+		# Use the inverse of trail_spacing, because it makes more sense that
+		# a larger number should give you more spacing, otherwise a value of
+		# 0.5 would give you more spacing than a value of 1, for instance.
+		var trail_spacing_inverse = 1.0 / trail_spacing
+		var trail_length = float(line_segments) * trail_spacing_inverse
+		trail_follow_2d.trail_offset = float(i) / trail_length - trail_spacing_inverse
+		trail_follow_2d.loop = false
+		trail_followers.append(trail_follow_2d)
 	
+	# Set the gradient of the root Line2D node to a new gradient object with
+	# line_segments number of color stops, all set to opaque white.
 	gradient = Gradient.new()
 	gradient.remove_point(0)
-
-	for g_cnt in range(line_segments):
-		gradient.add_point(float(g_cnt + 1) / float(line_segments), Color(1.0, 1.0, 1.0, 1.0))
+	
+	for i in range(line_segments):
+		gradient.add_point(float(i + 1) / float(line_segments), Color.white)
 
 
 func remove():
